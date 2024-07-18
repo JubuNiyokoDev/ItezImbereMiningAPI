54dc4df750b559c9efef32ef91fb88167f5d9e24
Jubu Niyoko Dev <134217666+JubuNiyokoDev@users.noreply.github.com>
2024-07-18 20:37:36 +0200

    -r

--- ItezImbereMining/settings.py
+++ ItezImbereMining/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p6gd#b1uu^cswgax$fs74b(b+rnoz^&np=zx0b&ooa()-f0g4z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
